568b468c0f8853105b8781dd9967cd001bfab473
mkarthikms <101816506+mkarthikms@users.noreply.github.com>
2022-03-18 20:17:17 +0530

    Update http_metrics.py

--- http_metrics.py
+++ http_metrics.py
@@ -1,6 +1,9 @@
 import socket
 import time
+import http.server
+import socketserver
 from urllib2 import urlopen
+from contextlib import redirect_stdout
 
 URLS = ['https://httpstat.us/200', 'https://httpstat.us/503']
 socket.setdefaulttimeout( 15 )  # timeout in seconds
@@ -17,6 +20,21 @@ for url in URLS:
            print 'sample_external_url_up {1} = {1}s\n'.format(time.clock() - start)'
         else:
 	   print ''
-   
+  
     except StandardError, e:
         print ''
+	
+with open('metrics', 'w') as f:
+    with redirect_stdout(f):
+       print('data')
+
+class httpmetrics(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'metrics.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+handler_object = httpmetrics
+httpserver = socketserver.TCPServer(("", 5000), handler_object)
+
+httpserver.serve_forever()
